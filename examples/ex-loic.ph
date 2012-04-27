3961d73b090af1bf5f012423d9cc847d1df8fd3e
Maxime Folschette <Jimi_Dave@hotmail.com>
2012-04-28 02:00:44 +0900

    Second commit

--- examples/ex-loic.ph
+++ examples/ex-loic.ph
@@ -17,9 +17,11 @@ process c 1
 
 process a 2
 
-COOPERATIVITY([b;c] -> d 1 0, [[0;0]])
-COOPERATIVITY([b;c] -> d 2 1, [[0;0];[0;1];[1;0]])
+COOPERATIVITY([b;c] -> a 1 0, [[0;0]])
+COOPERATIVITY([b;c] -> a 2 1, [[0;0];[0;1];[1;0]])
 
-COOPERATIVITY([b;c] -> d 0 1, [[0;1];[1;0];[1;1]])
-COOPERATIVITY([b;c] -> d 1 2, [[1;1]])
+COOPERATIVITY([b;c] -> a 0 1, [[0;1];[1;0];[1;1]])
+COOPERATIVITY([b;c] -> a 1 2, [[1;1]])
+
+a 2 -> b 1 0
 
--- ph2thomas.sh
+++ ph2thomas.sh
@@ -2,7 +2,7 @@
 
 if [ -z "$1" ]
 then
-  echo "Syntax: sh ph2thomas.sh <PH file> [ASP file [data file [DOT file [AB|active]]]]" >&2
+  echo "Syntax: sh ph2thomas.sh <PH file> [ASP file [data file [DOT file [AB|active|enum]]]]" >&2
   echo "The program computes as far as alowed by the given arguments" >&2
   exit 1
 fi
@@ -27,8 +27,8 @@ fi
 
 echo "Calling phinferIG.lp | phinferIG.ml ..." >&2
 clingo 0 --verbose=0 phinferIG.lp $2 \
-#| pint phinferIG.ml $2 $3 $4
-  exit 0
+| pint phinferIG.ml $2 $3 $4
+
 if [ -z "$5" ]
 then
   exit 0
@@ -37,4 +37,12 @@ fi
 echo "Calling phinferK.lp | phinferK.ml ..." >&2
 clingo 0 --verbose=0 phinferK.lp $2 \
 | pint phinferK.ml $2 $3 $5
+
+if [ "$5" != "enum" ]
+then
+  exit 0
+fi
+
+echo "Calling phenumK.lp ..." >&2
+clingo 0 --verbose=0 phenumK.lp $2
 
